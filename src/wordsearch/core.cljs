528dbeaf81fb99ef51bd85e00f36abb57ad78eb8
Dennis Heckman <denheck@gmail.com>
2017-04-15 10:47:17 -0500

    initial use of canvas for wordsearch board

--- src/wordsearch/core.cljs
+++ src/wordsearch/core.cljs
@@ -3,7 +3,7 @@
 
 (enable-console-print!)
 
-(println "This text is printed from src/wordsearch/core.cljs. Go ahead and edit it and see reloading in action.")
+;(println "This text is printed from src/wordsearch/core.cljs. Go ahead and edit it and see reloading in action.")
 
 ;; define your app data so that it doesn't get over-written on reload
 
@@ -12,26 +12,30 @@
     :title "Hello World"
     :tiles [
     [
-      { :letter "a", :in_words [] },
-      { :letter "b", :in_words [] },
-      { :letter "c", :in_words [] }
-    ],
+      { :letter "a" :in_words [] :pressed true }
+      { :letter "b" :in_words [] }
+      { :letter "c" :in_words [] }
+    ]
     [
-      { :letter "f", :in_words ["foo"] },
-      { :letter "o", :in_words ["foo"] },
-      { :letter "o", :in_words ["foo"] },
-    ],
+      { :letter "f" :in_words ["foo"] }
+      { :letter "o" :in_words ["foo"] }
+      { :letter "o" :in_words ["foo"] }
+    ]
     [
-      { :letter "d", :in_words [] },
-      { :letter "e", :in_words [] },
-      { :letter "f", :in_words [] }
+      { :letter "d" :in_words [] }
+      { :letter "e" :in_words [] }
+      { :letter "f" :in_words [] :pressed true }
     ]]}))
 
-(defn render-columns [columns] [:td]
-  (map (fn [column] [:td (:letter column)]) columns))
+(defn render-tiles [tiles] [:td]
+  (map (fn [tile] 
+         [(if (:pressed tile) 
+            :td.App-grid-item.pressed-grid-item 
+            :td.App-grid-item.unpressed-grid-item) 
+          (:letter tile)]) tiles))
 
 (defn render-rows [rows]
-  (map (fn [columns] [:tr (render-columns columns)]) rows))
+  (map (fn [tiles] [:tr (render-tiles tiles)]) rows))
 
 (defn app []
   [:div.App
@@ -50,6 +54,35 @@
 
 (reagent/render-component [app]
                           (. js/document (getElementById "app")))
+
+(def board-size 500)
+(def tile-size 25)
+;(def lines [[0 25 25 25] 
+;            [0 50 25 50]
+;            [0 75 25 75]
+;            [25 0 25 25]
+;            [50 0 50 25]
+;            [75 0 75 25]])
+(def lines (concat (for [x (range 0 board-size tile-size)] [x 0 x board-size])
+                   (for [y (range 0 board-size tile-size)] [0 y board-size y])))
+             
+
+(defn draw-line [context from-x from-y to-x to-y]
+  (. context (moveTo from-x from-y))
+  (. context (lineTo to-x to-y))
+  (set! (.-strokeStyle context) "black")
+  (. context stroke))
+
+(defn draw-board []
+  (let [canvas (. js/document (getElementById "board"))
+        context (. canvas getContext "2d")]
+    (loop [line (first lines)
+           lines (rest lines)]
+      (apply draw-line context line)
+      (if-not (empty? lines)
+        (recur (first lines) (rest lines))))))
+
+(draw-board)
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
